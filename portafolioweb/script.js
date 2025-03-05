04ce5ff2f23d27eef330a176dbf79ee8f181f844
ErlanJoseLC06 <123124442+ErlanJoseLC06@users.noreply.github.com>
2025-03-05 11:43:09 -0500

    Add files via upload

---
+++ portafolioweb/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000, // Duración de la animación
+        once: true, // Solo se anima una vez
+    });
+});
